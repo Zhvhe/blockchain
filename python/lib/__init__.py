83f9ebd37ef9d1dead792e9620e4cdd93fcc6ae9
zhvhe <zhvhem@gmail.com>
2022-11-12 17:53:53 -0600

    converted to notebook

--- python/lib/__init__.py
+++ python/lib/__init__.py
@@ -1,1 +1,21 @@
-__name__ = 'lib'
+#!/usr/bin/env python3
+
+import os, sys
+
+if(os.sep == '/'):
+    sys.path.insert(0, "../lib")
+    from .sign import *
+else:
+    sys.path.insert(0, "..\\lib")
+    from .sign_win import *
+
+from .base58 import *
+from .bech32 import *
+from .codes import *
+from .encoder import *
+from .format import *
+from .hash import *
+from .helper import *
+from .ripemd import *
+from .rpc import *
+
